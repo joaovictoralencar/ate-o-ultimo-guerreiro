594f98b5eff5f337be44cd0570debeee6bc8715a
João Victor de Alencar <joaovictoralencarpascoal@gmail.com>
2019-11-19 18:47:41 -0300

    removendo bt que não consegui utilizar

--- node_modules/behaviortree/src/decorators/index.js
+++
@@ -1,5 +0,0 @@
-export AlwaysFailDecorator from './AlwaysFailDecorator'
-export AlwaysSucceedDecorator from './AlwaysSucceedDecorator'
-export CooldownDecorator from './CooldownDecorator'
-export InvertDecorator from './InvertDecorator'
-export LoopDecorator from './LoopDecorator'
